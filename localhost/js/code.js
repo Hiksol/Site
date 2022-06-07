e153eb46a79407ee7c926008613ef99b658b22aa
Hiksol <92927696+Hiksol@users.noreply.github.com>
2022-06-07 23:29:34 +0300

    Add files via upload

---
+++ localhost/js/code.js
@@ -0,0 +1,12 @@
+
+$(function(){
+$('.nav , .main_start').mouseover(function (){
+  $(this).css("background-color","#828282");
+});
+$('.nav , .main_start').mouseout(function(){
+  $(this).css("background-color","#669933");
+});
+/*$('.main_start').focus(function(){
+  $(this).css("background-color","#828282");
+});*/
+});
---
+++ localhost/js/parse.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const jquery = require('jquery');
+const fs = require('fs');
+
+const parse = async () => {
+  const getHTML = async (url) => {
+    const  { data } = await axios.get(url);
+    return cheerio.load(data);
+  };
+  const $ = await getHTML('https://www.overclockers.ua/cpu/info/');
+  //console.log($.html());
+    const selector = await getHTML('https://www.overclockers.ua/cpu/info/');
+    selector('li').each((i,element) =>{
+        const title = selector(element).find('a').text();
+        const link_info =  selector(element).find('a').attr("href");
+   const link =  ("https://www.overclockers.ua" + `${link_info}`);
+    //  console.log(title);
+   console.log(link);
+    fs.appendFileSync('./data2.txt','/+`${link}\n`+/');
+});
+
+};
+
+
+parse();
